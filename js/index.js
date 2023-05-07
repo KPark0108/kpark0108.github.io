7e373433539cda9a69f5db81aa8a42f851cfad9d
KPark0108 <119399399+KPark0108@users.noreply.github.com>
2023-05-07 21:04:09 +1200

    member story section

--- js/index.js
+++ js/index.js
@@ -43,6 +43,33 @@ var swiper = new Swiper(".home-slider", {
     slidesPerView:'auto',
   });
 
+    //Story-slider
+
+    var swiper = new Swiper(".story-slider", {
+        spaceBetween: 20,
+        grabCursor: true,
+        loop:false,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints:{
+            0:{
+                slidesPerView:1,
+            },
+            768:{
+                slidesPerView:2,
+            },
+            991:{
+                slidesPerView:3,
+            },
+        },
+    });
+
   //TDEE Calculator
 
   const calculateButton = document.getElementById("calculate-button");
